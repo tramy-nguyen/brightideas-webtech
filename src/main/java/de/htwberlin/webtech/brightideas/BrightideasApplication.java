d31d88ca1f7ef9d04e1f56abb0715080e60dc052
tramy nguyen <tramy.nguyen@student-htw-berlin.de>
2022-04-22 09:18:25 +0200

    first commit

---
+++ src/main/java/de/htwberlin/webtech/brightideas/BrightideasApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.brightideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightideasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrightideasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech/brightideas/BrightideasApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.brightideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrightideasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
