8086de8fe488674560f897c552ed3f0cd2360e4d
Hoang Anh Nguyen <xxxjun.l.0ng@gmail.com>
2022-05-14 22:00:15 +0200

    modified README and add FlashcardManipulationRequest

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
+[![Java CI with Gradle](https://github.com/tramy-nguyen/brightideas-webtech/actions/workflows/tests.yml/badge.svg)](https://github.com/tramy-nguyen/brightideas-webtech/actions/workflows/tests.yml)[![Java CI with Gradle](https://github.com/tramy-nguyen/brightideas-webtech/actions/workflows/tests.yml/badge.svg)](https://github.com/tramy-nguyen/brightideas-webtech/actions/workflows/tests.yml)
+
 # brightideas-webtech
 ***
-Description 
+Description
+A website that offers flashcards on different topics for individual learning purposes.
+Most of the time flashcards will contain a question, an answer and different options to choose an answer from.
+These flashcards will be sorted in different categories.
+User is able to learn with flashcards in the database or create his/her own flashcards.
+User created flashcards will be added to the database and will be usable for every user.
 
-a website for creating flashcards that supports images
+Status 
+Can deploy to Heroku and has a Rest-API.
+
+Next Step
+Complete backend and start working on the frontend with Vue.js
---
+++ src/main/java/de/htwberlin/webtech/brightideas/web/FlashcardManipulationRequest.java
@@ -0,0 +1,58 @@
+package de.htwberlin.webtech.brightideas.web;
+
+public class FlashcardCreateRequest {
+
+    private String question;
+    private String answer;
+    private String options;
+    private boolean flip;
+    private String category;
+
+    public FlashcardCreateRequest(String question, String answer, String options, boolean flip, String category) {
+        this.question = question;
+        this.answer = answer;
+        this.options = options;
+        this.flip = flip;
+        this.category = category;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getOptions() {
+        return options;
+    }
+
+    public void setOptions(String options) {
+        this.options = options;
+    }
+
+    public boolean isFlip() {
+        return flip;
+    }
+
+    public void setFlip(boolean flip) {
+        this.flip = flip;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
